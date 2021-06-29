90602d3c07d59d199e28396a496c148e690e03d7
pepe199111 <piotrpustul1@gmail.com>
2021-06-29 11:18:17 +0100

    worked on authentication, added signup link, post and get signup conttroller, installed bcrytpjs npm packaged

--- controllers/auth.js
+++ controllers/auth.js
@@ -8,6 +8,14 @@ exports.getLogin = (req, res, next) => {
    });
 };
 
+exports.getSignup = (req, res, next) => {
+   res.render('auth/signup', {
+      path: '/signup',
+      pageTitle: 'Signup',
+      isAuthenticated: false
+   });
+};
+
 exports.postLogin = (req, res, next) => {
    User.findById('60d3455b0282ca3d69af24c0')
       .then(user => {
@@ -17,6 +25,29 @@ exports.postLogin = (req, res, next) => {
             console.log(err);
             res.redirect('/');
          });
+      })
+      .catch(err => console.log(err));
+};
+
+exports.postSignup = (req, res, next) => {
+   const email = req.body.email;
+   const password = req.body.password;
+   const confirmPassword = req.body.confirmPassword;
+
+   User.findOne({ email: email })
+      .then(userDoc => {
+         if (userDoc) {
+            return res.redirect('/signup');
+         }
+         const user = new User({
+            email: email,
+            password: password,
+            cart: { items: [] }
+         })
+         return user.save();
+      })
+      .then(result => {
+         res.redirect('/login')
       })
       .catch(err => console.log(err));
 };
