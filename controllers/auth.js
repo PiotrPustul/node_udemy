15baea119f22d7f5f74a8cc31461a3133ac8797a
Piotr Pustul <pepe@Piotrs-MBP.cust.communityfibre.co.uk>
2021-06-29 12:07:31 +0100

    added middleware is-auth to block access to the routes

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,5 @@
+const bcrypt = require('bcryptjs');
+
 const User = require('../models/user');
 
 exports.getLogin = (req, res, next) => {
@@ -17,14 +19,31 @@ exports.getSignup = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-   User.findById('60d3455b0282ca3d69af24c0')
+   const email = req.body.email;
+   const password = req.body.password;
+
+   User.findOne({ email: email })
       .then(user => {
-         req.session.isLoggedIn = true;
-         req.user = user;
-         req.session.save((err) => {
-            console.log(err);
-            res.redirect('/');
-         });
+         if (!user) {
+            return res.redirect('/login');
+         }
+         bcrypt
+            .compare(password, user.password)
+            .then(doMatch => {
+               if (doMatch) {
+                  req.session.isLoggedIn = true;
+                  req.user = user;
+                  return req.session.save((err) => {
+                     console.log(err);
+                     res.redirect('/');
+                  });
+               }
+               res.redirect('/login');
+            })
+            .catch(err => {
+               console.log(err);
+               res.redirect('/login');
+            });
       })
       .catch(err => console.log(err));
 };
@@ -39,15 +58,19 @@ exports.postSignup = (req, res, next) => {
          if (userDoc) {
             return res.redirect('/signup');
          }
-         const user = new User({
-            email: email,
-            password: password,
-            cart: { items: [] }
-         })
-         return user.save();
-      })
-      .then(result => {
-         res.redirect('/login')
+         return bcrypt
+            .hash(password, 12)
+            .then(hashPassword => {
+               const user = new User({
+                  email: email,
+                  password: hashPassword,
+                  cart: { items: [] }
+               })
+               return user.save();
+            })
+            .then(result => {
+               res.redirect('/login')
+            });
       })
       .catch(err => console.log(err));
 };
