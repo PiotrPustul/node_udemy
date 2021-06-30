64ef95c18080317a1b7d8b1d429e9c81d8f104b6
Piotr Pustul <pepe@Piotrs-MacBook-Pro.local>
2021-06-30 15:18:52 +0100

    added async validation

--- controllers/auth.js
+++ controllers/auth.js
@@ -43,10 +43,10 @@ exports.getSignup = (req, res, next) => {
   });
 };
 
+
 exports.postLogin = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
-
   User.findOne({ email: email })
     .then(user => {
       if (!user) {
@@ -78,48 +78,35 @@ exports.postLogin = (req, res, next) => {
 exports.postSignup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
-  const confirmPassword = req.body.confirmPassword;
+
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     console.log(errors.array());
-
-    return res
-      .status(422)
-      .render('auth/signup', {
-        path: '/signup',
-        pageTitle: 'Signup',
-        errorMessage: errors.array()[0].msg
-      });
+    return res.status(422).render('auth/signup', {
+      path: '/signup',
+      pageTitle: 'Signup',
+      errorMessage: errors.array()[0].msg
+    });
   }
 
-  User.findOne({ email: email })
-    .then(userDoc => {
-      if (userDoc) {
-        req.flash('error', 'E-mail exists already.');
-        return res.redirect('/signup');
-      }
-      return bcrypt
-        .hash(password, 12)
-        .then(hashedPassword => {
-          const user = new User({
-            email: email,
-            password: hashedPassword,
-            cart: { items: [] }
-          });
-          return user.save();
-        })
-        .then(result => {
-          res.redirect('/login');
-          return transporter.sendMail({
-            to: email,
-            from: 'piotrpustul1@gmail.com',
-            subject: 'Singup Succeeded!.',
-            html: '<h1>You successfully Signed Up!.</h1>'
-          });
-        })
-        .catch(err => {
-          console.log(err);
-        });
+  bcrypt
+    .hash(password, 12)
+    .then(hashedPassword => {
+      const user = new User({
+        email: email,
+        password: hashedPassword,
+        cart: { items: [] }
+      });
+      return user.save();
+    })
+    .then(result => {
+      res.redirect('/login');
+      // return transporter.sendMail({
+      //   to: email,
+      //   from: 'shop@node-complete.com',
+      //   subject: 'Signup succeeded!',
+      //   html: '<h1>You successfully signed up!</h1>'
+      // });
     })
     .catch(err => {
       console.log(err);
--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { check, body } = require('express-validator');
 
 const authController = require('../controllers/auth');
+const User = require('../models/user');
 
 const router = express.Router();
 
@@ -16,20 +17,32 @@ router.post(
    [
       check('email')
          .isEmail()
-         .withMessage('Please enter a valid email'),
+         .withMessage('Please enter a valid email.')
+         .custom((value, { req }) => {
+            // if (value === 'test@test.com') {
+            //   throw new Error('This email address if forbidden.');
+            // }
+            // return true;
+            return User.findOne({ email: value }).then(userDoc => {
+               if (userDoc) {
+                  return Promise.reject(
+                     'E-Mail exists already, please pick a different one.'
+                  );
+               }
+            });
+         }),
       body(
          'password',
          'Please enter a password with only numbers and text and at least 5 characters.'
       )
          .isLength({ min: 5 })
          .isAlphanumeric(),
-      body('confirmPassword')
-         .custom((value, { req }) => {
-            if (value !== req.body.password) {
-               throw new Error('Passwords have to match!.')
-            }
-            return true;
-         })
+      body('confirmPassword').custom((value, { req }) => {
+         if (value !== req.body.password) {
+            throw new Error('Passwords have to match!');
+         }
+         return true;
+      })
    ],
    authController.postSignup
 );
